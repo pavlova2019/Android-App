71fa6e2a7f7054454a8ee68f19bcef139a56363c
Alexandra Pavlova <pavlovaalexandra29@gmail.com>
2021-10-13 18:41:34 +0300

    Base navigation

---
+++ app/src/main/java/com/alexandrapavlova/mydumbapp/repository/AuthRepository.kt
@@ -0,0 +1,17 @@
+package com.alexandrapavlova.mydumbapp.repository
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+object AuthRepository {
+    private val _isAuthorizedFlow = MutableStateFlow(false)
+    val isAuthorizedFlow = _isAuthorizedFlow.asStateFlow()
+
+    suspend fun signIn() {
+        _isAuthorizedFlow.emit(true)
+    }
+
+    suspend fun logOut() {
+        _isAuthorizedFlow.emit(false)
+    }
+}
--- app/src/main/java/com/alexandrapavlova/mydumbapp/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/alexandrapavlova/mydumbapp/ui/signin/SignInFragment.kt
@@ -1,6 +1,9 @@
 package com.alexandrapavlova.mydumbapp.ui.signin
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.alexandrapavlova.mydumbapp.R
 import com.alexandrapavlova.mydumbapp.databinding.FragmentSignInBinding
@@ -11,5 +14,15 @@ class SignInFragment: BaseFragment(R.layout.fragment_sign_in) {
     private val viewModel: SignInViewModel by viewModels()
 
     private val viewBinding by viewBinding(FragmentSignInBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        viewBinding.signInButton.setOnClickListener {
+            viewModel.signIn()
+        }
+    }
 
 }
